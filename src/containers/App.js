8b877a35c4177629f41338c6636fd718f5a64b7a
brianplynn <brnlynn889@gmail.com>
2019-01-19 18:17:59 -0800

    never gon lose

---
+++ src/containers/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import Header from "../components/Header.js";
+import Main from "../components/Main.js";
+
+const App = () => {
+    return (
+      <div className="App bg-black-80">
+      	<Header />
+      	<Main />
+      </div>
+    );
+}
+
+export default App;
+
+/* STRUCTURE:
+
+Nurtrition: graph on the left side that can show caloric intake / 
+protein  / fat / carb intake over 7 / 30 days
+
+on the right  an input box that connects to edamame api to search for 
+foods. once found and submitted, adds to a table of foods already eaten 
+that day, which add up to a total 
+
+Sleep:
+
+sleep graph plus an input box for how many hours, emoji to 
+demonsrate how the sleep went
+
+Exercise:
+
+You are on day x of your 7/14-day plan! 2x7 table. current day's workout listed below with optino to edit.
+clicking on another day lets you update that day
+
+
+Things in my state object:
+
+date
+which module i've clicked on
+
+nutrition:
+all the foods i've eaten that day stored as an array of objects, 
+with nutrients as keys
+
+value of each input field (auto and manual)
+
+also could transition between calories counter
+
+sleep:
+my sleep for last week, stored as an array of objects with length
+and quality as keys
+
+current sleep value and quality (based on input checkbox and field)
+
+exercise:
+ current workout (based on day)
+ current day of workout
+
+
+*/
+
---
+++ src/containers/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		  <nav class="bt bb tc mw7 center mt4">
+		    <span class="f6 f5-l link bg-animate light-gray hover-bg-gray dib pa3 ph4-l" 
+		       href="">Nutrition</span>
+		    <span class="f6 f5-l link bg-animate light-gray hover-bg-gray dib pa3 ph4-l" 
+		       href="">Sleep</span>
+		    <span class="f6 f5-l link bg-animate light-gray hover-bg-gray dib pa3 ph4-l" 
+		       href="">Exercise</span>   
+		  </nav>
+		)
+}
+
+export default Navbar;
