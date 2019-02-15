f154110dca0e2facec185edf9bcb8bef1575b9cb
brianplynn <brnlynn889@gmail.com>
2019-02-14 16:46:52 -0800

    added github

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Link, browserHistory } from "react-router-dom"
 import "./Login.css";
 import history from "../history";
-import FacebookLogin from 'react-facebook-login';
-import { logInFB } from "../actions";
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+import GitHubLogin from 'react-github-login';
 
 
 class Login extends Component {
-	
+
 	responseFacebook = (response) => {
 	  const { logInFacebook } = this.props;
 	  console.log(response);
@@ -18,18 +17,44 @@ class Login extends Component {
 	  // fetch login
 	  // fetch register if fails
 	}
+
+	onSuccess = response => {
+		fetch('https://github.com/login/oauth/access_token', {
+	        method: "post",
+	        headers: {'Content-Type': 'application/json'},
+	        body: JSON.stringify({  
+	          client_id: 'c7bdc63f0a88829cb6f2',
+	          client_secret: '81e4ae8221fd323b103bab4cca15433b26f42ff4',
+	          code: response
+	        })
+	      })
+		.then(res => res.json())
+		.then(res => console.log(res))
+	}
+    onFailure = response => console.error(response);
+
 	render() {
 		return (
 			<div className="login center flex flex-column justify-center ba ph3 pv3 pv4-ns ph4-m ph5-l">
 			  <h1 className="white tc">Welcome!</h1>
-				<div className="facebook mt2 mb5">
+				<div className="facebook mt2">
 				<FacebookLogin
 				    appId="513697765824552"
-				    autoLoad={true}
 				    fields="name,email"
+				    render={renderProps => (
+						    <button className="facebook-btn"
+						    		onClick={renderProps.onClick}>
+						    		Sign in with Facebook</button>
+						  )}
 				    callback={this.responseFacebook}
 				    version={3.2} />
 				</div>
+				<GitHubLogin className="github"
+							clientId="c7bdc63f0a88829cb6f2"
+							buttonText="Sign in with Github"
+							redirectUri=""
+						    onSuccess={this.onSuccess}
+						    onFailure={this.onFailure}/>
 			</div>
 		);	
 	}
